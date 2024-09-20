d46153771f117ad39fd812da7907efd72ce0592d
Charles <charlesmarque.work@gmail.com>
2024-09-20 15:40:38 +0200

    terraform week10

---
+++ backend.tf
@@ -0,0 +1,8 @@
+#terraform {
+# backend "s3" {
+#  bucket = "week10-vpc-ec2-alb"   #replace with your bucket
+# key    = "week10/terraform.tfstate"
+#region = "us-east-1"
+#encrypt = true
+#dynamodb_table = "locktable" #replace with the table
+
